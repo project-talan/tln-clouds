b6c58afaff7845190421529ceddde1054513c183
VladyslavKurmaz <vladislav.kurmaz@gmail.com>
2022-11-28 20:34:52 +0000

    feat: bump do provider version

--- do/tenant/versions.tf
+++ do/tenant/versions.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = "= 1.2.6"
+  required_version = "= 1.3.5"
 
   required_providers {
     digitalocean = {
       source = "digitalocean/digitalocean"
-      version = "2.22.1"
+      version = "2.24.0"
     }
   }
 }
