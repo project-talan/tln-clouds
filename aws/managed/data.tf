0b34839840766b586b4eba460430cfb2285289ab
msherstnov-cdf <133634326+msherstnov-cdf@users.noreply.github.com>
2023-11-01 22:21:53 +0200

    aws: move bastion to network layer (#26)
    Co-authored-by: PROPVGVNDA <nekitshero@gmail.com>

--- aws/managed/data.tf
+++ aws/managed/data.tf
@@ -13,3 +13,17 @@ data "aws_subnets" "private" {
 data "aws_subnets" "public" {
   tags = merge(module.shared.tags, module.shared.public_subnet_tags)
 }
+
+data "aws_security_group" "bastion" {
+  filter {
+    name   = "tag:Name"
+    values = ["${var.env_id}-bastion-sg"]
+  }
+
+  filter {
+    name   = "tag:Env"
+    values = [var.env_id]
+  }
+
+  vpc_id = data.aws_vpc.main.id
+}
--- aws/network/bastion.tf
+++ aws/network/bastion.tf
@@ -1,9 +1,9 @@
 data "aws_ami" "ubuntu" {
-  most_recent = true
+  most_recent = false
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-20231030"]
   }
 
   filter {
@@ -14,25 +14,17 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
-resource "aws_eip" "bastion" {
-  instance = aws_instance.bastion.id
-  domain   = "vpc"
-}
-
 resource "aws_instance" "bastion" {
   ami                         = data.aws_ami.ubuntu.id
   instance_type               = "t3.micro"
-  subnet_id                   = data.aws_subnets.public.ids[0]
+  subnet_id                   = module.vpc.public_subnets[0]
   associate_public_ip_address = true
 
   vpc_security_group_ids = [module.bastion_sg.security_group_id]
   key_name               = aws_key_pair.ssh.key_name
 
-  user_data                   = base64encode(file("${path.module}/templates/template.sh"))
-  user_data_replace_on_change = true
-
   tags = {
-    Name = "bastion"
+    Name = "${var.env_id}-bastion"
   }
 }
 
@@ -40,9 +32,9 @@ module "bastion_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.1.0"
 
-  name        = "bastion-sg"
+  name        = "${var.env_id}-bastion-sg"
   description = "Security group for web-server to allow SSH access"
-  vpc_id      = data.aws_vpc.main.id
+  vpc_id      = module.vpc.vpc_id
 
   ingress_with_cidr_blocks = [
     {
