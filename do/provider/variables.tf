62402c2752c36e10887a40c504653e2eae685337
VladyslavKurmaz <vladislav.kurmaz@gmail.com>
2023-11-03 15:28:43 +0200

    feat(do): provider layer

--- do/provider/variables.tf
+++ do/provider/variables.tf
@@ -1,13 +1,7 @@
 variable "org_id"     { type = string }
 variable "project_id" { type = string }
+variable "group_id"   { type = string }
 variable "env_id"     { type = string }
 variable "tenant_id"  { type = string }
 
 variable "do_region"  { type = string }
-
-/*
-variable "domain_tag" { type = string }
-variable "private_key" { type = string }
-variable "certificate_chain" { type = string }
-variable "leaf_certificate" { type = string }
-*/
--- do/provider/versions.tf
+++ do/provider/versions.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = "= 1.5.7"
+  required_version = "= 1.6.3"
 
   required_providers {
     digitalocean = {
       source = "digitalocean/digitalocean"
-      version = "2.30.0"
+      version = "2.31.0"
     }
   }
 }
