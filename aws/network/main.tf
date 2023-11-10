64f1d18c6b971df2074a92163ac707a4b6a1c29f
VladyslavKurmaz <vladislav.kurmaz@gmail.com>
2023-11-10 22:58:05 +0200

    feat(aws): add db subnets

--- aws/network/main.tf
+++ aws/network/main.tf
@@ -16,6 +16,7 @@ module "vpc" {
 
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  database_subnets = ["10.0.7.0/24", "10.0.8.0/24", "10.0.9.0/24"]
 
   enable_nat_gateway   = true
   single_nat_gateway   = true
