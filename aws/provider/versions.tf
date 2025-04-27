b62ffa119a905df7b26c4f8bf56f92bebc718754
VladyslavKurmaz <vladislav.kurmaz@gmail.com>
2025-04-27 23:10:58 +0300

    feat: reconstruct components

--- aws/provider/versions.tf
+++ aws/provider/versions.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = "= 1.9.8"
+  required_version = "= 1.11.4"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.74.0"
+      version = "5.96.0"
     }
   }
 }
---
+++ shared/variables.tf
@@ -0,0 +1,18 @@
+variable "org_id" {
+  type = string
+}
+variable "project_id" {
+  type = string
+}
+variable "group_id" {
+  type = string
+  default = ""
+}
+variable "env_id" {
+  type = string
+  default = ""
+}
+variable "tenant_id" {
+  type = string
+  default = ""
+}
