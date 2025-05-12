169673c2b2a4110bb93604a616fda5bc9a2b4281
VladyslavKurmaz <vladislav.kurmaz@gmail.com>
2025-05-12 13:37:26 +0300

    feat(aws): Refresh SES configuration - done

--- aws/group/ses.tf
+++ aws/group/ses.tf
@@ -1,9 +1,6 @@
-// Uncomment the following code if you want to enable SES (Simple Email Service)
-
-/*
 module "ses" {
   source  = "cloudposse/ses/aws"
-  version = "0.25.0"
+  version = "0.25.1"
 
   name = "${module.shared.prefix_group}-ses"
   custom_from_subdomain = ["no-reply"]
@@ -130,7 +127,7 @@ variable "context" {
 
 variable "enabled" {
   type        = bool
-  default     = true
+  default     = null
   description = "Set to false to prevent the module from creating any resources"
 }
 
@@ -311,49 +308,3 @@ variable "descriptor_formats" {
 }
 
 #### End of copy of cloudposse/terraform-null-label/variables.tf
-#------------------------------------------------------------------------------
-#------------------------------------------------------------------------------
-
-output "ses_domain_identity_arn" {
-  value       = module.ses.ses_domain_identity_arn
-  description = "The ARN of the SES domain identity."
-}
-
-output "ses_domain_identity_verification_token" {
-  value       = module.ses.ses_domain_identity_verification_token
-  description = "A code which when added to the domain as a TXT record will signal to SES that the owner of the domain has authorised SES to act on their behalf. The domain identity will be in state 'verification pending' until this is done. See below for an example of how this might be achieved when the domain is hosted in Route 53 and managed by Terraform. Find out more about verifying domains in Amazon SES in the AWS SES docs."
-}
-
-output "user_name" {
-  value       = module.ses.user_name
-  description = "Normalized IAM user name."
-}
-
-output "user_arn" {
-  value       = module.ses.user_arn
-  description = "The ARN assigned by AWS for this user."
-}
-
-output "user_unique_id" {
-  value       = module.ses.user_unique_id
-  description = "The unique ID assigned by AWS."
-}
-
-output "secret_access_key" {
-  sensitive   = true
-  value       = module.ses.secret_access_key
-  description = "The IAM secret for usage with SES API. This will be written to the state file in plain text."
-}
-
-# https://docs.aws.amazon.com/ses/latest/DeveloperGuide/smtp-credentials.html
-output "ses_smtp_password" {
-  sensitive   = true
-  value       = module.ses.ses_smtp_password
-  description = "The SMTP password. This will be written to the state file in plain text."
-}
-
-output "access_key_id" {
-  value       = module.ses.access_key_id
-  description = "The SMTP user which is access key ID."
-}
-*/
