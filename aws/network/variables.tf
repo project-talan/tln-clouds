438fdbaedcc48ab47d0a31cb25a63452462208a0
VladyslavKurmaz <vladislav.kurmaz@gmail.com>
2025-05-14 09:12:28 +0300

    feat(aws): bastion default packages

--- aws/network/variables.tf
+++ aws/network/variables.tf
@@ -35,6 +35,11 @@ variable "database_subnets" {
   type    = list(string)
   default = ["10.0.7.0/24", "10.0.8.0/24", "10.0.9.0/24"]
 }
+
 variable "bastion_custom_packages" {
   type        = map(string)
+  default     = {
+    kubectl: "1.3x.x"
+    helm: "3.xx.x"
+  }
 }
