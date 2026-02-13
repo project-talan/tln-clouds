2b8034d0b2eb9c86eeb0bb685ea4e6fe379e229f
rommatdevops <matiakoupov@gmail.com>
2026-02-13 16:12:09 +0200

    feat(aws): set nginx values to values.yml

--- aws/app/nginx.tf
+++ aws/app/nginx.tf
@@ -5,39 +5,9 @@ resource "helm_release" "nginx" {
   lint             = true
   repository       = "https://kubernetes.github.io/ingress-nginx"
   chart            = "ingress-nginx"
-  version          = "4.12.2"
+  version          = "4.14.3"
 
   set = [
-    {
-      name  = "controller.replicaCount"
-      value = 2
-      type  = "auto"
-    },
-    {
-      name  = "controller.nodeSelector.kubernetes\\.io/os"
-      value = "linux"
-      type  = "string"
-    },
-    {
-      name  = "defaultBackend.nodeSelector.kubernetes\\.io/os"
-      value = "linux"
-      type  = "string"
-    },
-    {
-      name  = "controller.admissionWebhooks.patch.nodeSelector.kubernetes\\.io/os"
-      value = "linux"
-      type  = "string"
-    },
-    {
-      name  = "controller.config.annotations-risk-level"
-      value = "Critical"
-      type  = "string"
-    },
-    {
-      name  = "controller.config.use-forwarded-headers"
-      value = "true"
-      type  = "string"
-    },
     {
       name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-ssl-cert"
       value = var.use_primary_domain ? data.aws_acm_certificate.primary.arn : module.secondary_certificate.acm_certificate_arn
