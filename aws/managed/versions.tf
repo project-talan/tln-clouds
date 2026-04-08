0ae48225ab27e90c31c8392256a5b5ddd03ec5f5
tostapovych-datami <t.ostapovych@datami.ee>
2026-04-08 18:36:58 +0300

    feat(aws/005):upgrade managed level, upgrade network level with three NATs
    edited according to POC example, addons was first of all removed from web interface, and then recreated via terraform  cli console.

--- aws/managed/versions.tf
+++ aws/managed/versions.tf
@@ -3,22 +3,22 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.96.0"
+      version = "6.39.0"
     }
 
     tls = {
       source  = "hashicorp/tls"
-      version = "4.0.6"
+      version = "4.2.1"
     }
 
     cloudinit = {
       source  = "hashicorp/cloudinit"
-      version = "2.3.6"
+      version = "2.3.7"
     }
 
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "2.36.0"
+      version = "3.0.1"
     }
   }
 }
--- aws/network/main.tf
+++ aws/network/main.tf
@@ -19,8 +19,9 @@ module "vpc" {
   database_subnets = var.database_subnets
 
   enable_nat_gateway   = true
-  single_nat_gateway   = true
+  #single_nat_gateway   = true
   enable_dns_hostnames = true
+  #enable_dns_support   = true
 
   public_subnet_tags = merge(module.shared.public_subnet_tags, {
     "kubernetes.io/cluster/${module.shared.k8s_name}" = "shared"
