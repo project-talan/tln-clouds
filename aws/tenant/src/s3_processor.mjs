7cc58dd43d9c6547d025a38d61aa4dd2c40a19ac
tostapovych-datami <t.ostapovych@datami.ee>
2026-04-30 18:14:36 +0300

    feat(aws/016)change the process env variables

--- aws/tenant/src/s3_processor.mjs
+++ aws/tenant/src/s3_processor.mjs
@@ -1,6 +1,6 @@
 export const handler = async (event) => {
     // URL вашого сервісу в EKS (Internal Load Balancer або ClusterIP з VPC Lattice)
-    const EKS_SERVICE_URL = "http://cluster.local";
+    const EKS_SERVICE_URL = process.env.EKS_SERVICE_URL;
 
     for (const record of event.Records) {
         const bucket = record.s3.bucket.name;
