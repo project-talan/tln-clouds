0c009fe736e6058e209806d4c9952e2ee95f93d1
tostapovych-datami <t.ostapovych@datami.ee>
2026-06-16 11:07:21 +0300

    feat(aws/012) refactor main db config

--- aws/shared/db_config/main-db.tf
+++ aws/shared/db_config/main-db.tf
@@ -9,7 +9,7 @@ provider "postgresql" {
   superuser       = false
 }
 
-resource "postgresql_role" "this" {
+resource "postgresql_role" "main" {
   provider = postgresql.main-db
   for_each = var.databases
 
@@ -19,47 +19,47 @@ resource "postgresql_role" "this" {
 
 }
 
-resource "postgresql_database" "this" {
+resource "postgresql_database" "main" {
   provider = postgresql.main-db
   for_each = var.databases
 
   name              = each.key
-  owner             = postgresql_role.this[each.key].name
+  owner             = postgresql_role.main[each.key].name
   template          = "template0"
   lc_collate        = "en_US.UTF-8"
   connection_limit  = -1
   allow_connections = true
 
-  depends_on = [postgresql_role.this]
+  depends_on = [postgresql_role.main]
 }
 
-resource "postgresql_grant" "this_table" {
+resource "postgresql_grant" "main_table" {
   provider = postgresql.main-db
   for_each = var.databases
 
-  database    = postgresql_database.this[each.key].name
-  role        = postgresql_role.this[each.key].name
+  database    = postgresql_database.main[each.key].name
+  role        = postgresql_role.main[each.key].name
   schema      = "public"
   object_type = "table"
   privileges  = ["ALL"]
 
-  depends_on = [postgresql_database.this]
+  depends_on = [postgresql_database.main]
   lifecycle {
     ignore_changes = [privileges] # To prevent Terraform from revoking manually granted privileges
   }
 }
 
-resource "postgresql_grant" "this_schema" {
+resource "postgresql_grant" "main_schema" {
   provider = postgresql.main-db
   for_each = var.databases
 
-  database    = postgresql_database.this[each.key].name
-  role        = postgresql_role.this[each.key].name
+  database    = postgresql_database.main[each.key].name
+  role        = postgresql_role.main[each.key].name
   schema      = "public"
   object_type = "schema"
   privileges  = ["CREATE", "USAGE"] # Grant CREATE and USAGE on public schema
 
-  depends_on = [postgresql_database.this]
+  depends_on = [postgresql_database.main]
   lifecycle {
     ignore_changes = [privileges]
   }
